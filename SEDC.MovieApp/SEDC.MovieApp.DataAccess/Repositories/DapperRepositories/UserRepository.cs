de3da962fc0e66e6f853d9ee9d2dd00f6cb8e4f5
Darijan Sekerov <darijan2002@gmail.com>
2021-10-04 23:04:12 +0200

    user repository

--- SEDC.MovieApp/SEDC.MovieApp.DataAccess/Repositories/DapperRepositories/UserRepository.cs
+++ SEDC.MovieApp/SEDC.MovieApp.DataAccess/Repositories/DapperRepositories/UserRepository.cs
@@ -1,41 +1,65 @@
 ﻿using Dapper;
+using Dapper.Contrib.Extensions;
+//using Dapper.Contrib;
 using SEDC.MovieApp.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace SEDC.MovieApp.DataAccess.Repositories.DapperRepositories
 {
     public class UserRepository : IUserRepository
     {
+        private const string ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=MovieDB;Integrated Security=True";
+
         public int Create(User entity)
         {
-            throw new NotImplementedException();
+            string hashed;
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] passwordBytes = Encoding.ASCII.GetBytes(entity.Password);
+                List<byte> hashedBytes = md5.ComputeHash(passwordBytes).ToList();
+                StringBuilder sb = new StringBuilder();
+                hashedBytes.ForEach(b => sb.Append(b.ToString("X2")));
+                hashed = sb.ToString();
+            }
+            using IDbConnection db = new SqlConnection(ConnectionString);
+            return db.QuerySingle<int>(@"
+INSERT INTO [User] ([Username], [Password]) 
+OUTPUT INSERTED.[UserId] 
+VALUES (@username, @password);", new { username = entity.Username, password = hashed });
         }
 
         public IEnumerable<User> GetAll()
         {
             IEnumerable<User> users; // = new List<User>();
-            using(IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=MovieDB;Integrated Security=True"))
-            {
-                users = db.Query<User>("SELECT [UserId], [Username] FROM [User]");
-                
-            }
+            using IDbConnection db = new SqlConnection(ConnectionString);
+            users = db.Query<User>("SELECT [UserId], [Username] FROM [User];");
 
             return users;
         }
 
         public User GetById(int id)
         {
-            throw new NotImplementedException();
+            using IDbConnection db = new SqlConnection(ConnectionString);
+            return db.QuerySingleOrDefault<User>(@"
+SELECT [UserId], [Username] 
+FROM [User] 
+WHERE [UserId] = @id;", new { id });
         }
 
         public User GetByUsername(string username)
         {
-            throw new NotImplementedException();
+            using IDbConnection db = new SqlConnection(ConnectionString);
+            return db.QuerySingleOrDefault<User>(@"
+SELECT [UserId], [Username]
+FROM [User] 
+WHERE [Username] = @username;", new { username });
         }
     }
 }
